1f25723dc51d9d62c01a8c0767ea415fe33f4882
dgpatelgit <dhpatel@redhat.com>
2020-12-01 18:32:31 +0530

    Added usage for 4 optional param

--- gomanifest/internal/golist.go
+++ gomanifest/internal/golist.go
@@ -1,12 +1,8 @@
 package internal
 
 import (
-	"bytes"
 	"io"
 	"os/exec"
-	"strings"
-
-	"github.com/rs/zerolog/log"
 )
 
 // GoListCmd ... Go list command structure.
@@ -28,26 +24,25 @@ func GetGoExecutable() (*GoListCmd, error) {
 }
 
 // RunGoList ... Actual function that executes go list command and returns output as string.
-func RunGoList(cwd string, goExe string) (*GoListCmd, error) {
-	cmd, err := GetGoExecutable()
-	if err != nil {
-		log.Error().Err(err).Msg("`which go` failed")
-		return nil, err
-	}
+func RunGoList(cwd string, goExec string) (*GoListCmd, error) {
+	/*	cmd, err := GetGoExecutable()
+		if err != nil {
+			log.Error().Err(err).Msg("`which go` failed")
+			return nil, err
+		}
 
-	defer cmd.ReadCloser().Close()
-	buf := new(bytes.Buffer)
-	buf.ReadFrom(cmd.ReadCloser())
-	s := strings.TrimSpace(buf.String())
-	log.Info().Msg("Found go executable " + s)
+		defer cmd.ReadCloser().Close()
+		buf := new(bytes.Buffer)
+		buf.ReadFrom(cmd.ReadCloser())
+		s := strings.TrimSpace(buf.String())
+		log.Info().Msg("Found go executable " + s)
 
-	// Wait for the `go list` command to complete.
-	//if err := cmd.Wait(); err != nil {
-	//	return nil, fmt.Errorf("%v: `go list` failed, use `go mod tidy` to known more", err)
-	//}
-
-	log.Info().Msg("Passed go executable " + goExe)
-	goList := exec.Command(goExe, "list", "-json", "-deps", "-mod=readonly", "./...")
+		// Wait for the `go list` command to complete.
+		//if err := cmd.Wait(); err != nil {
+		//	return nil, fmt.Errorf("%v: `go list` failed, use `go mod tidy` to known more", err)
+		//}
+	*/
+	goList := exec.Command(goExec, "list", "-json", "-deps", "-mod=readonly", "./...")
 	goList.Dir = cwd
 	output, err := goList.StdoutPipe()
 	if err != nil {
